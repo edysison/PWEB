6bcaa7afccce7a27fe138725993be7e716226421
Eduardo Son <eson@iouu.com.br>
2020-05-18 21:19:17 -0300

    Entrega da atividade03

---
+++ Atividade03/media.js
@@ -0,0 +1,4 @@
+let nota1 = parseFloat(window.prompt("Nota 1"));
+let nota2 = parseFloat(window.prompt("Nota 2"));
+let nota3= parseFloat(window.prompt("Nota 3"));
+alert("Media de "(nota1+nota2+nota3)/3)
---
+++ Atividade03/operacoes.js
@@ -0,0 +1,7 @@
+let numero1 = parseFloat(window.prompt("Numero 1"));
+let numero2 = parseFloat(window.prompt("Numero 2"));
+alert("Soma "+(numero1+numero2))
+alert("Subtração "+(numero1-numero2))
+alert("Produto "+(numero1*numero2))
+alert("Divisão "+(numero1/numero2))
+alert("Resto da divisão "+(numero1%numero2))
